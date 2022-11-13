cfb91070206832c7e6a5ccd85bf307ec36743da4
abdulsaabir <saabirhaaji10@gmail.com>
2022-11-13 09:08:25 -0800

    done

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-import waait from "https://esm.sh/waait";
-async function consollog() {
-  await waait(50);
-  console.log("hello");
-  await waait(8000);
-  console.log("jabroni");
-}
-consollog();
+let menuItems = document.querySelectorAll(".menu-item ");
+let notificationsPopup = document.querySelector(".notifications-popup ");
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", (e) => {
+    let currentTarget = e.currentTarget;
+    currentTarget.childElementCount > 2
+      ? notificationsPopup.classList.toggle("activeNow")
+      : notificationsPopup.classList.remove("activeNow");
+
+    if (currentTarget.classList.contains("active")) return;
+    menuItems.forEach((menuitemnow) => menuitemnow.classList.remove("active"));
+    currentTarget.classList.add("active");
+  });
+});
