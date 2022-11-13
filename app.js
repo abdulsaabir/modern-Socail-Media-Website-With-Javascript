a7b105bf7e3fa66afd0769462c664b77b3fbaf9c
abdulsaabir <saabirhaaji10@gmail.com>
2022-11-13 11:18:50 -0800

    Middle section done

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+let backgroundArray = [
+  "url('./images/story-1.jpg')",
+  "url('./images/story-2.jpg')",
+  "url('./images/story-3.jpg')",
+  "url('./images/story-4.jpg')",
+  "url('./images/story-5.jpg')",
+  "url('./images/story-6.jpg')",
+];
+let photosArray = [
+  "./images/feed-1.jpg",
+  "./images/feed-2.jpg",
+  "./images/feed-3.jpg",
+  "./images/feed-4.jpg",
+  "./images/feed-5.jpg",
+  "./images/feed-6.jpg",
+  "./images/feed-7.jpg",
+];
+
 let menuItems = document.querySelectorAll(".menu-item ");
 let notificationsPopup = document.querySelector(".notifications-popup ");
+let stories = document.querySelectorAll(".story");
+let photos = document.querySelectorAll(".imagesphoto");
+
+notificationsPopup.classList.remove("activeNow");
 
 menuItems.forEach((menuItem) => {
   menuItem.addEventListener("click", (e) => {
@@ -12,4 +34,12 @@ menuItems.forEach((menuItem) => {
     menuItems.forEach((menuitemnow) => menuitemnow.classList.remove("active"));
     currentTarget.classList.add("active");
   });
+});
+
+stories.forEach((story, index) => {
+  story.style.backgroundImage = backgroundArray[index];
+});
+
+photos.forEach((photo, index) => {
+  photo.src = photosArray[index];
 });
