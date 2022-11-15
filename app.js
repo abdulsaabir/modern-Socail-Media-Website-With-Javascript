c8730731b019b9a41fac10c8734b79cd472622e2
abdulsaabir <saabirhaaji10@gmail.com>
2022-11-15 11:19:22 -0800

    javascript Done

--- app.js
+++ app.js
@@ -17,10 +17,31 @@ let photosArray = [
 ];
 let messages = [
   {
-    url: "./images/feed-1.jpg",
+    url: "./images/profile-15.jpg",
     name: "hassan kaafi",
     text: "goormee iimaane",
     active: true,
+    read: false,
+  },
+  {
+    url: "./images/profile-4.jpg",
+    name: "Julien pete",
+    text: "good night babe",
+    active: false,
+    read: true,
+  },
+  {
+    url: "./images/profile-9.jpg",
+    name: "Yusuf mohamuud",
+    text: "netflix bassword iisheeg",
+    active: true,
+    read: false,
+  },
+  {
+    url: "./images/profile-18.jpg",
+    name: "Sudlaani yare",
+    text: "Hi macalin",
+    active: false,
     read: false,
   },
 ];
@@ -29,7 +50,7 @@ let notificationsPopup = document.querySelector(".notifications-popup ");
 let stories = document.querySelectorAll(".story");
 let photos = document.querySelectorAll(".imagesphoto");
 let messagesList = document.querySelector(".messagesList");
-
+let searchMessage = document.getElementById("search-message");
 notificationsPopup.classList.remove("activeNow");
 
 menuItems.forEach((menuItem) => {
@@ -51,4 +72,37 @@ stories.forEach((story, index) => {
 
 photos.forEach((photo, index) => {
   photo.src = photosArray[index];
+});
+window.addEventListener("DOMContentLoaded", () => {
+  Loadmessage(messages);
+});
+
+function Loadmessage(array) {
+  messagesList.innerHTML = "";
+  array.forEach((message) => {
+    let messagenow = document.createElement("div");
+    messagenow.innerHTML = `
+  <div class="message">
+  <div class="profile-photo">
+      <img src=${message.url}>
+      ${message.active ? '<div class="active"></div>' : ""}
+  </div>
+  <div class="message-body">
+  <h5>${message.name}</h5>
+      <p class=${message.read ? "text-muted" : "text-bold"}>${message.text}</p>
+      </div>
+      </div>`;
+    messagesList.appendChild(messagenow);
+  });
+}
+searchMessage.addEventListener("keyup", (e) => {
+  let value = searchMessage.value;
+  let FiltredMessages = messages.filter((message) => {
+    let name = message.name.toLocaleLowerCase();
+    if (name.includes(value)) {
+      console.log();
+      return message;
+    }
+  });
+  Loadmessage(FiltredMessages);
 });
